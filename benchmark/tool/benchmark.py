a06c228bf62548fe61f30a55a11d91f24b240591
mikerainey <bionic.oldsmobile@gmail.com>
2021-10-03 10:52:56 -0400

    adding error handling

--- benchmark/tool/benchmark.py
+++ benchmark/tool/benchmark.py
@@ -1,4 +1,4 @@
-import jsonschema, subprocess, tempfile, os, sys, socket
+import jsonschema, subprocess, tempfile, os, sys, socket, signal, threading, time
 import simplejson as json
 from datetime import datetime
 from parameter import *
@@ -89,6 +89,21 @@ def seed_benchmark(benchmark_1):
         benchmark_2['todo'] = eval(benchmark_1['parameters'])
     return benchmark_2
 
+_currentChild = None
+def _signalHandler(signal, frame):
+  sys.stderr.write("[ERR] Interrupted.\n")
+  if _currentChild:
+    _currentChild.kill()
+  sys.exit(1)
+signal.signal(signal.SIGINT, _signalHandler)
+
+def _killer():
+  if _currentChild:
+    try:
+      _currentChild.kill()
+    except Exception as e:
+      sys.stderr.write("[WARN] Error while trying to kill process {}: {}\n".format(_currentChild.pid, str(e)))
+
 def run_benchmark(cmd, env_args, cwd = ''):
     if cwd == '':
         return subprocess.Popen(cmd, shell = True, env = env_args,
@@ -98,7 +113,9 @@ def run_benchmark(cmd, env_args, cwd = ''):
                                 stdout = subprocess.PIPE, stderr = subprocess.PIPE, cwd = cwd)
 
 def step_benchmark_1(benchmark_1, verbose = True,
-                     client_format_to_row = lambda d: dictionary_to_row(d)):
+                     client_format_to_row = lambda d: dictionary_to_row(d),
+                     parse_output_to_json = lambda x: x,
+                     timeout_sec = 0.0):
     jsonschema.validate(benchmark_1, benchmark_schema)
     parameters = benchmark_1['parameters']
     modifiers = benchmark_1['modifiers']
@@ -128,24 +145,37 @@ def step_benchmark_1(benchmark_1, verbose = True,
     current_child = run_benchmark(string_of_benchmark_run(next_run),
                                   { a['var']: str(a['val']) for a in next_run['benchmark_run']['env_args'] },
                                   modifiers['cwd'] if 'cwd' in modifiers else '')
-    so, se = current_child.communicate()
-    rc = current_child.returncode
-    # Collect the results
-    results_expr = {'value': [ next_row ]}
-    for of in outfiles:
-        fd = open(of['file_name'], 'r')
-        j = json.load(fd)
-        open(of['file_name'], 'w').close()
-        results_expr = mk_cross(results_expr, {'value': [client_format_to_row(d) for d in j] })
-        os.unlink(of['file_name'])
+    timer = threading.Timer(timeout_sec, _killer) if timeout_sec > 0.0 else -1
+    ts = time.time()
+    child_stdout = ''
+    child_stderr = ''
+    try:
+        if timer != -1:
+            timer.start()
+        child_stdout, child_stderr = current_child.communicate()
+        # Collect the results
+        results_expr = {'value': [ next_row ]}
+        for of in outfiles:
+            j = json.load(parse_output_to_json(open(of['file_name'], 'r')))
+            open(of['file_name'], 'w').close()
+            os.unlink(of['file_name'])
+            results_expr = mk_cross(results_expr, {'value': [client_format_to_row(d) for d in j] })
+        benchmark_2['todo'] = { 'value': todo_2 }
+        benchmark_2['done'] = eval(mk_append(benchmark_1['done'], results_expr))
+        return_code = current_child.returncode
+    finally:
+        if timer != -1:
+            timer.cancel()
+        return_code = 1
     # Save the results
     trace_2 = next_run.copy()
-    trace_2['benchmark_run']['return_code'] = rc
+    trace_2['benchmark_run']['return_code'] = return_code
     trace_2['benchmark_run']['timestamp'] = datetime.now().strftime("%y-%m-%d %H:%M:%S.%f")
     trace_2['benchmark_run']['host'] = socket.gethostname()
-    benchmark_2['todo'] = { 'value': todo_2 }
+    trace_2['benchmark_run']['elapsed'] = time.time() - ts
+    trace_2['benchmark_run']['stdout'] = str(child_stdout)
+    trace_2['benchmark_run']['stderr'] = str(child_stderr)
     benchmark_2['trace'] += [ trace_2 ]
-    benchmark_2['done'] = eval(mk_append(benchmark_1['done'], results_expr))
     jsonschema.validate(benchmark_2, benchmark_schema)
     return benchmark_2
 
