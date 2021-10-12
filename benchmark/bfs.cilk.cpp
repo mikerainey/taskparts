1a4484b3dc15059f7ef1b3143dad3e33eb0574fa
mikerainey <bionic.oldsmobile@gmail.com>
2021-10-12 19:53:36 -0400

    edit

--- benchmark/bfs.cilk.cpp
+++ benchmark/bfs.cilk.cpp
@@ -2,14 +2,14 @@
 #include "bfs.hpp"
 
 int main() {
-  auto fname = taskparts::cmdline::parse_or_default_string("fname", "randlocal.adj");
+  auto infile = taskparts::cmdline::parse_or_default_string("infile", "randlocal.adj");
   Graph G;
   sequence<vertexId> parents;
   size_t source = 0;
   taskparts::benchmark_cilk([&] { // benchmark
     parents = BFS(source, G);
   }, [&] { // setup
-    G = readGraphFromFile<vertexId,edgeId>((char*)fname.c_str());
+    G = readGraphFromFile<vertexId,edgeId>((char*)infile.c_str());
     G.addDegrees();
   }, [&] { // teardown
     size_t visited = parlay::reduce(parlay::delayed_map(parents, [&] (auto p) -> size_t {
