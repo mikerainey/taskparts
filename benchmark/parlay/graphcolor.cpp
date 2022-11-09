01e4439b8654a33de483b52fb468107aabc8350f
Ubuntu <ubuntu@ip-172-31-64-40.ec2.internal>
2022-11-09 21:11:38 +0000

    using different input for graphcolor

--- benchmark/parlay/graphcolor.cpp
+++ benchmark/parlay/graphcolor.cpp
@@ -4,15 +4,19 @@
 parlay::sequence<int> colors;
 
 auto gen_input2() {
+  std::string infile_path = "";
+  if (const auto env_p = std::getenv("TASKPARTS_BENCHMARK_INFILE_PATH")) {
+    infile_path = std::string(env_p);
+  }
   force_sequential = taskparts::cmdline::parse_or_default_bool("force_sequential", false);
   parlay::override_granularity = taskparts::cmdline::parse_or_default_long("override_granularity", 0);
   include_infile_load = taskparts::cmdline::parse_or_default_bool("include_infile_load", false);
   n = std::max((size_t)1, (size_t)taskparts::cmdline::parse_or_default_long("n", 4 * 1000 * 1000));
-  source = taskparts::cmdline::parse_or_default_long("source", source);
-  auto input = taskparts::cmdline::parse_or_default_string("input", "rmat");
-  auto infile = input + ".adj";
-  if (n == 0) {
-    G = utils::read_symmetric_graph_from_file(infile.c_str());
+  source = taskparts::cmdline::parse_or_default_long("source", 1);
+  auto input = taskparts::cmdline::parse_or_default_string("input", "europe");
+  if (input != "gen-rmat") {
+    auto infile = infile_path + "/" + input + ".adj";
+    G = utils::read_graph_from_file_pbbs(infile.c_str());
     n = G.size();
   } else {
     G = utils::rmat_symmetric_graph(n, 20*n);
