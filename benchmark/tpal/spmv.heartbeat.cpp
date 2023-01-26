4c8fcd4281f0453de65e3a5bcb9735e32b2f7f22
Ubuntu <ubuntu@ip-172-31-32-107.us-east-2.compute.internal>
2023-01-26 03:46:55 +0000

    refactoring srad and floyd_warshall

---
+++ benchmark/tpal/spmv.heartbeat.cpp
@@ -0,0 +1,181 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdint>
+#include "rollforward.h"
+
+#define D 64
+
+void row_loop_spawn(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t row_lo,
+  uint64_t row_hi);
+void col_loop_spawn(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t row_lo,
+  uint64_t row_hi,
+  uint64_t col_lo,
+  uint64_t col_hi,
+  double t, uint64_t nb_rows);
+void col_loop_col_loop_spawn(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t col_lo,
+  uint64_t col_hi,
+  double t,
+  double* dst);
+
+void rollforward_handler_annotation __rf_handle_row_loop(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t row_lo,
+  uint64_t row_hi, bool& promoted) {
+  
+  if ((row_hi - row_lo) <= 1) {
+    promoted = false;
+  } else {
+    row_loop_spawn(val, row_ptr, col_ind, x, y, row_lo, row_hi);    
+    promoted = true;
+  }
+  rollbackward
+}
+
+void rollforward_handler_annotation __rf_handle_col_loop(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t row_lo,
+  uint64_t row_hi,
+  uint64_t col_lo,
+  uint64_t col_hi,
+  double t, bool& promoted) {
+  
+  auto nb_rows = row_hi - row_lo;
+  if (nb_rows == 0) {
+    promoted = false;
+  } else {
+    promoted = true;
+    col_loop_spawn(val, row_ptr, col_ind, x, y, row_lo, row_hi, col_lo, col_hi, t, nb_rows);
+  }
+ exit:
+  rollbackward
+}
+
+void rollforward_handler_annotation __rf_handle_col_loop_col_loop(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t col_lo,
+  uint64_t col_hi,
+  double t,
+  double* dst, bool& promoted) {
+  
+  if ((col_hi - col_lo) <= 1) {
+    promoted = false;
+  } else {
+    promoted = true;
+    col_loop_col_loop_spawn(val, row_ptr, col_ind, x, y, col_lo, col_hi, t, dst);
+  }
+  rollbackward
+}
+
+#define D 1024
+
+void spmv_interrupt(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t row_lo,
+  uint64_t row_hi) {
+  if (! (row_lo < row_hi)) { // row loop
+    return;
+  }
+  for (; ; ) { 
+    double r = 0.0;
+    uint64_t col_lo = row_ptr[row_lo];
+    uint64_t col_hi = row_ptr[row_lo + 1];
+    if (! (col_lo < col_hi)) { // col loop (1)
+      goto exit_col_loop;
+    }
+    for (; ; ) {
+      uint64_t col_lo2 = col_lo;
+      uint64_t col_hi2 = std::min(col_lo + D, col_hi);
+      for (; col_lo2 < col_hi2; col_lo2++) { // col loop (2)
+        r += val[col_lo2] * x[col_ind[col_lo2]];
+      }
+      col_lo = col_lo2;
+      if (! (col_lo < col_hi)) {
+        break;
+      }
+      bool promoted = false;
+      __rf_handle_col_loop(val, row_ptr, col_ind, x, y, row_lo, row_hi, col_lo, col_hi, r, promoted);
+      if (rollforward_branch_unlikely(promoted)) {
+	return;
+      }
+    }
+  exit_col_loop:
+    y[row_lo] = r;
+    row_lo++;
+    if (! (row_lo < row_hi)) {
+      break;
+    }
+    bool promoted = false;
+    __rf_handle_row_loop(val, row_ptr, col_ind, x, y, row_lo, row_hi, promoted);
+    if (rollforward_branch_unlikely(promoted)) {
+      return;
+    }
+  }
+}
+
+void spmv_interrupt_col_loop(
+  double* val,
+  uint64_t* row_ptr,
+  uint64_t* col_ind,
+  double* x,
+  double* y,
+  uint64_t col_lo,
+  uint64_t col_hi,
+  double r,
+  double* dst) {
+  if (! (col_lo < col_hi)) {
+    goto exit;
+  }
+  for (; ; ) {
+    uint64_t col_lo2 = col_lo;
+    uint64_t col_hi2 = std::min(col_lo + D, col_hi);
+    for (; col_lo2 < col_hi2; col_lo2++) {
+      r += val[col_lo2] * x[col_ind[col_lo2]];
+    }
+    col_lo = col_lo2;
+    if (! (col_lo < col_hi)) {
+      break;
+    }
+    bool promoted = false;
+    __rf_handle_col_loop_col_loop(val, row_ptr, col_ind, x, y, col_lo, col_hi, r, dst, promoted);
+    if (rollforward_branch_unlikely(promoted)) {
+      return;
+    }
+  }
+exit:
+  *dst = r;
+}
+
--- benchmark/tpal/srad.cpp
+++ benchmark/tpal/srad.cpp
@@ -31,10 +31,7 @@ void srad_interrupt_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo,
 void srad_interrupt_inner_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda);
 void srad_interrupt_inner_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda);
 
-/* Handler functions */
-/* ================= */
-
-void rollforward_handler_annotation __rf_handle_srad_0(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
+void handle_srad_0(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
   auto nb_rows = rows_hi - rows_lo;
   if (nb_rows <= 1) {
     promoted = false;
@@ -62,10 +59,9 @@ void rollforward_handler_annotation __rf_handle_srad_0(int rows, int cols, int r
     }
     promoted = true;
   }
-  rollbackward
 }
 
-void rollforward_handler_annotation __rf_handle_srad_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
+void handle_srad_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
   auto nb_rows = rows_hi - rows_lo;
   if (nb_rows <= 1) {
     promoted = false;
@@ -90,10 +86,9 @@ void rollforward_handler_annotation __rf_handle_srad_1(int rows, int cols, int r
     }
     promoted = true;
   }
-  rollbackward
 }
 
-void rollforward_handler_annotation __rf_handle_srad_inner_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
+void handle_srad_inner_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
   if ((cols_hi - cols_lo) <= 1) {
     promoted = false;
   } else {
@@ -105,10 +100,9 @@ void rollforward_handler_annotation __rf_handle_srad_inner_1(int rows, int cols,
     }, [&] {}, taskparts::bench_scheduler());
     promoted = true;
   }
-  rollbackward
 }
 
-void rollforward_handler_annotation __rf_handle_srad_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
+void handle_srad_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
   auto nb_rows = rows_hi - rows_lo;
   if (nb_rows <= 1) {
     promoted = false;
@@ -134,10 +128,9 @@ void rollforward_handler_annotation __rf_handle_srad_2(int rows, int cols, int r
     }
     promoted = true;
   }
-  rollbackward
 }
 
-void rollforward_handler_annotation __rf_handle_srad_inner_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
+void handle_srad_inner_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda, bool& promoted) {
   if ((cols_hi - cols_lo) <= 1) {
     promoted = false;
   } else {
@@ -148,265 +141,6 @@ void rollforward_handler_annotation __rf_handle_srad_inner_2(int rows, int cols,
       srad_interrupt_inner_2(rows, cols, rows_lo, rows_hi, cols_mid, cols_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda);
     }, [&] {}, taskparts::bench_scheduler());
     promoted = true;
-  }
-  rollbackward
-}
-
-/* Outlined-loop functions */
-/* ======================= */
-
-#define DT 64
-
-void srad_interrupt(int rows, int cols, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
-  for (int i = 0 ; i < rows ; i++) {
-    int col_lo = 0;
-    int col_hi = cols;
-    if (! (col_lo < col_hi)) {
-      continue;
-    }
-    for (;;) {
-      int col_lo2 = col_lo;
-      int col_hi2 = std::min(col_lo + DT, col_hi);
-      for (; col_lo2 < col_hi2; col_lo2++) {
-	int j = col_lo2;
-	int k = i * cols + j;
-	float Jc = J[k];
-
-	// directional derivates
-	dN[k] = J[iN[i] * cols + j] - Jc;
-	dS[k] = J[iS[i] * cols + j] - Jc;
-	dW[k] = J[i * cols + jW[j]] - Jc;
-	dE[k] = J[i * cols + jE[j]] - Jc;
-
-	float G2 = (dN[k]*dN[k] + dS[k]*dS[k] 
-		    + dW[k]*dW[k] + dE[k]*dE[k]) / (Jc*Jc);
-
-	float L = (dN[k] + dS[k] + dW[k] + dE[k]) / Jc;
-
-	float num  = (0.5*G2) - ((1.0/16.0)*(L*L)) ;
-	float den  = 1 + (.25*L);
-	float qsqr = num/(den*den);
-
-	// diffusion coefficent (equ 33)
-	den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
-	c[k] = 1.0 / (1.0+den) ;
-
-	// saturate diffusion coefficent
-	if (c[k] < 0) {c[k] = 0;}
-	else if (c[k] > 1) {c[k] = 1;}
-      }
-      col_lo = col_lo2;
-      if (! (col_lo < col_hi)) {
-        break;
-      }
-      bool promoted = false;
-      __rf_handle_srad_0(rows, cols, i, rows, col_lo, col_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda, promoted);
-      if (rollforward_branch_unlikely(promoted)) {
-	return;
-      }
-
-    }
-  }
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {        
-
-      // current index
-      int k = i * cols + j;
-                
-      // diffusion coefficent
-      float cN = c[k];
-      float cS = c[iS[i] * cols + j];
-      float cW = c[k];
-      float cE = c[i * cols + jE[j]];
-
-      // divergence (equ 58)
-      float D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
-                
-      // image update (equ 61)
-      J[k] = J[k] + 0.25*lambda*D;
-    }
-  }
-}
-
-void srad_interrupt_1(int rows, int cols, int rows_lo, int rows_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
-  for (int i = rows_lo ; i < rows_hi ; i++) {
-    int col_lo = 0;
-    int col_hi = cols;
-    if (! (col_lo < col_hi)) {
-      continue;
-    }
-    for (;;) {
-      int col_lo2 = col_lo;
-      int col_hi2 = std::min(col_lo + DT, col_hi);
-      for (; col_lo2 < col_hi2; col_lo2++) {
-	int j = col_lo2;
-	int k = i * cols + j;
-	float Jc = J[k];
-
-	// directional derivates
-	dN[k] = J[iN[i] * cols + j] - Jc;
-	dS[k] = J[iS[i] * cols + j] - Jc;
-	dW[k] = J[i * cols + jW[j]] - Jc;
-	dE[k] = J[i * cols + jE[j]] - Jc;
-
-	float G2 = (dN[k]*dN[k] + dS[k]*dS[k] 
-		    + dW[k]*dW[k] + dE[k]*dE[k]) / (Jc*Jc);
-
-	float L = (dN[k] + dS[k] + dW[k] + dE[k]) / Jc;
-
-	float num  = (0.5*G2) - ((1.0/16.0)*(L*L)) ;
-	float den  = 1 + (.25*L);
-	float qsqr = num/(den*den);
-
-	// diffusion coefficent (equ 33)
-	den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
-	c[k] = 1.0 / (1.0+den) ;
-
-	// saturate diffusion coefficent
-	if (c[k] < 0) {c[k] = 0;}
-	else if (c[k] > 1) {c[k] = 1;}
-      }
-      col_lo = col_lo2;
-      if (! (col_lo < col_hi)) {
-        break;
-      }
-      bool promoted = false;
-      __rf_handle_srad_1(rows, cols, i, rows_hi, col_lo, col_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda, promoted);
-      if (rollforward_branch_unlikely(promoted)) {
-	return;
-      }
-    }
-  }
-}
-
-void srad_interrupt_inner_1(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
-  int i = rows_lo;
-  int col_lo = cols_lo;
-  int col_hi = cols_hi;
-  if (! (col_lo < col_hi)) {
-    return;
-  }
-  for (;;) {
-    int col_lo2 = col_lo;
-    int col_hi2 = std::min(col_lo + DT, col_hi);
-    for (; col_lo2 < col_hi2; col_lo2++) {
-      int j = col_lo2;
-      int k = i * cols + j;
-      float Jc = J[k];
-
-      // directional derivates
-      dN[k] = J[iN[i] * cols + j] - Jc;
-      dS[k] = J[iS[i] * cols + j] - Jc;
-      dW[k] = J[i * cols + jW[j]] - Jc;
-      dE[k] = J[i * cols + jE[j]] - Jc;
-
-      float G2 = (dN[k]*dN[k] + dS[k]*dS[k] 
-		  + dW[k]*dW[k] + dE[k]*dE[k]) / (Jc*Jc);
-
-      float L = (dN[k] + dS[k] + dW[k] + dE[k]) / Jc;
-
-      float num  = (0.5*G2) - ((1.0/16.0)*(L*L)) ;
-      float den  = 1 + (.25*L);
-      float qsqr = num/(den*den);
-
-      // diffusion coefficent (equ 33)
-      den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
-      c[k] = 1.0 / (1.0+den) ;
-
-      // saturate diffusion coefficent
-      if (c[k] < 0) {c[k] = 0;}
-      else if (c[k] > 1) {c[k] = 1;}
-    }
-    col_lo = col_lo2;
-    if (! (col_lo < col_hi)) {
-      break;
-    }
-    bool promoted = false;
-    __rf_handle_srad_inner_1(rows, cols, rows_lo, rows_hi, col_lo, col_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda, promoted);
-    if (rollforward_branch_unlikely(promoted)) {
-      return;
-    }
-  }
-}
-
-void srad_interrupt_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
-  for (int i = rows_lo; i < rows_hi; i++) {
-    int col_lo = 0;
-    int col_hi = cols;
-    if (! (col_lo < col_hi)) {
-      continue;
-    }
-    for (;;) {
-      int col_lo2 = col_lo;
-      int col_hi2 = std::min(col_lo + DT, col_hi);
-      for (; col_lo2 < col_hi2; col_lo2++) {
-	int j = col_lo2;
-
-	// current index
-	int k = i * cols + j;
-
-	// diffusion coefficent
-	float cN = c[k];
-	float cS = c[iS[i] * cols + j];
-	float cW = c[k];
-	float cE = c[i * cols + jE[j]];
-
-	// divergence (equ 58)
-	float D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
-
-	// image update (equ 61)
-	J[k] = J[k] + 0.25*lambda*D;
-      }
-      col_lo = col_lo2;
-      if (! (col_lo < col_hi)) {
-	break;
-      }
-      bool promoted = false;
-      __rf_handle_srad_2(rows, cols, i, rows_hi, col_lo, col_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda, promoted);
-      if (rollforward_branch_unlikely(promoted)) {
-	return;
-      }
-    }
-  }
-}
-
-void srad_interrupt_inner_2(int rows, int cols, int rows_lo, int rows_hi, int cols_lo, int cols_hi, int size_I, int size_R, float* I, float* J, float q0sqr, float *dN, float *dS, float *dW, float *dE, float* c, int* iN, int* iS, int* jE, int* jW, float lambda) {
-  int i = rows_lo;
-  int col_lo = cols_lo;
-  int col_hi = cols_hi;
-  if (! (col_lo < col_hi)) {
-    return;
-  }
-  for (;;) {
-    int col_lo2 = col_lo;
-    int col_hi2 = std::min(col_lo + DT, col_hi);
-    for (; col_lo2 < col_hi2; col_lo2++) {
-      int j = col_lo2;
-
-      // current index
-      int k = i * cols + j;
-
-      // diffusion coefficent
-      float cN = c[k];
-      float cS = c[iS[i] * cols + j];
-      float cW = c[k];
-      float cE = c[i * cols + jE[j]];
-
-      // divergence (equ 58)
-      float D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
-
-      // image update (equ 61)
-      J[k] = J[k] + 0.25*lambda*D;
-    }
-    col_lo = col_lo2;
-    if (! (col_lo < col_hi)) {
-      break;
-    }
-    bool promoted = false;
-    __rf_handle_srad_inner_2(rows, cols, rows_lo, rows_hi, col_lo, col_hi, size_I, size_R, I, J, q0sqr, dN, dS, dW, dE, c, iN, iS, jE, jW, lambda, promoted);
-    if (rollforward_branch_unlikely(promoted)) {
-      return;
-    }
   }
 }
 
