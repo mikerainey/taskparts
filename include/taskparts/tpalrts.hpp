6be75b254ba3606a5c1f9a7cf62f9cf1883e336c
Ubuntu <ubuntu@ip-172-31-32-107.us-east-2.compute.internal>
2023-05-04 18:16:59 +0000

    fix for performance bug in tpalrts (relating to order in which new tasks are pushed on the deque) and optimization to avoid expensive atomic increments

--- include/taskparts/tpalrts.hpp
+++ include/taskparts/tpalrts.hpp
@@ -10,6 +10,8 @@ namespace taskparts {
 /*---------------------------------------------------------------------*/
 /* Promotion */
 
+#if 0
+
 template <typename F1, typename F2, typename Fj, typename Scheduler>
 auto tpalrts_promote_via_nativefj(const F1& f1, const F2& f2, const Fj& fj, Scheduler sched) {
   using fiber_type = nativefj_fiber<Scheduler>;
@@ -21,8 +23,8 @@ auto tpalrts_promote_via_nativefj(const F1& f1, const F2& f2, const Fj& fj, Sche
   fiber<Scheduler>::add_edge(&fb1, &fbj);
   fiber<Scheduler>::add_edge(&fb2, &fbj);
   fiber<Scheduler>::add_edge(&fbj, cfb);
-  fb1.release();
   fb2.release();
+  fb1.release();
   fbj.release();
   if (context::capture<fiber_type*>(context::addr(cfb->ctx))) {
     return;
@@ -42,6 +44,42 @@ auto tpalrts_promote_via_nativefj(const F1& f1, const F2& f2, const Fj& fj, Sche
   cfb->status = fiber_status_finish;
   cfb->swap_with_scheduler();
 }
+
+#else
+
+template <typename F1, typename F2, typename Fj, typename Scheduler>
+auto tpalrts_promote_via_nativefj(const F1& f1, const F2& f2, const Fj& fj, Scheduler sched) {
+  using fiber_type = nativefj_fiber<Scheduler>;
+  nativefj_from_lambda<decltype(f1), Scheduler> fb1(f1);
+  nativefj_from_lambda<decltype(f2), Scheduler> fb2(f2);
+  nativefj_from_lambda<decltype(fj), Scheduler> fbj(fj);
+  auto cfb = fiber_type::current_fiber.mine();
+  cfb->status = fiber_status_pause;
+  fb1.outedge = &fbj; fb2.outedge = &fbj;
+  fbj.outedge = cfb; cfb->incounter.store(1);
+  fb2.incounter.store(0); fb2.schedule();
+  fb1.incounter.store(0); fb1.schedule();
+  fbj.incounter.store(2);
+  if (context::capture<fiber_type*>(context::addr(cfb->ctx))) {
+    return;
+  }
+  fb1.stack = fiber_type::notownstackptr;
+  fb1.swap_with_scheduler();
+  fb1.run();
+  auto f = Scheduler::template take<nativefj_fiber>();
+  if (f == nullptr) {
+    cfb->status = fiber_status_finish;
+    cfb->exit_to_scheduler();
+    return; // unreachable
+  }
+  fb2.stack = fiber_type::notownstackptr;
+  fb2.swap_with_scheduler();
+  fb2.run();
+  cfb->status = fiber_status_finish;
+  cfb->swap_with_scheduler();
+}
+
+#endif
 
 /*---------------------------------------------------------------------*/
 /* Promotion mark list */
