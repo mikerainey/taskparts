e56523482dfcecaaa67160c893bdab1a79fd09a2
mikerainey <bionic.oldsmobile@gmail.com>
2023-06-05 16:33:55 -0400

    threshold

--- benchmark/fib.cpp
+++ benchmark/fib.cpp
@@ -10,6 +10,9 @@ int main(int argc, char* argv[]) {
     try { n = std::stol(argv[1]); }
     catch (...) { printf("%s\n", usage); }
     long r;
+    if (const auto env_p = std::getenv("FIB_THRESHOLD")) {
+      fib_serial_threshold = std::max(2l, std::stol(env_p));
+    }
     taskparts::benchmark([&] {
       r = fib_nativeforkjoin(n);
     });
